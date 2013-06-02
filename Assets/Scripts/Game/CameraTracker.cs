16cd246df07d33518f54f115383bfe25cebb262e
Kazumitsu Tsuchida <kazumitsu@pankaku.com>
2013-06-02 23:09:30 +0900

    カメラディレイトラッッキング

--- Assets/Scripts/Game/CameraTracker.cs
+++ Assets/Scripts/Game/CameraTracker.cs
@@ -2,18 +2,34 @@ using UnityEngine;
 using System.Collections;
 
 public class CameraTracker : MonoBehaviour {
-	
 	public GameObject target;
 	public float lerpStep = 10f;
 
 	// Update is called once per frame
 	void Update () {
-		LerpTarget();
+		//LerpTarget();
 	}
 	
-	void LerpTarget()
+	public void LerpTarget()
 	{
-		MoveTarget(Vector3.Lerp(this.gameObject.transform.position, target.transform.position, lerpStep));
+		fromPos = transform.position;
+		toPos = target.transform.position.SetX( fromPos.x).SetZ( fromPos.z);
+		StartCoroutine("Track", 0.7f);
+		//MoveTarget(Vector3.Lerp(this.gameObject.transform.position.SetX(0), target.transform.position.SetX(0), lerpStep));
+	}
+	
+	Vector3 fromPos;
+	Vector3 toPos;
+	
+	IEnumerator Track (float time){
+		transform.position = fromPos;
+		
+		float t = 0;
+		while( transform.position != toPos){
+			transform.position = Vector3.Lerp( fromPos, toPos, t);
+			t += Time.deltaTime/time;
+			yield return null;
+		}
 	}
 	
 	void MoveTarget(Vector3 to)
--- Assets/Scripts/Game/StageObjectGenerator.cs
+++ Assets/Scripts/Game/StageObjectGenerator.cs
@@ -7,32 +7,45 @@ public class StageObjectGenerator : MonoBehaviour, IObjserver {
 	[SerializeField] List<GameObject> objectPrefabs = new List<GameObject>();
 	List<GameObject> stageObjects = new List<GameObject>();
 	
+	
+	//TODO SEPARATE
+	[SerializeField] CameraTracker cameraTracker;
+	
 	// Use this for initialization
 	void Start () {
 		GenerateHigherStep();
 	}
 	
-	public void GenerateHigherStep(){
+	public GameObject GenerateHigherStep(){
 		if( stageObjects.Count == 0)
-			Generate( Vector3.zero);
+			return Generate( Vector3.zero);
 		else{
 			var highest = FindHighest();
 			var newPos = ThinkHigherPostion( highest.transform.position);
-			Generate( newPos);
+			return Generate( newPos);
 		}
 	}
 	
 	public void Notify( CoStageObject cso){
 		Debug.Log( "notify "+cso.gameObject.name);
 		if( FindHighest() == cso.gameObject){
-			GenerateHigherStep();
+			
+			var generated = GenerateHigherStep();
+			
+			var highestScreenPos = cameraTracker.camera.WorldToScreenPoint( cso.transform.position);
+			var screenHeight = cameraTracker.camera.GetScreenHeight();
+			if( screenHeight * 0.7 < highestScreenPos.y){
+				cameraTracker.LerpTarget();
+			}
+				
 		}
 	}
 	
 	Vector3 ThinkHigherPostion( Vector3 pos){
 		return pos
 			.SetX( UnityEngine.Random.Range( -2.2f, 2.2f))
-				.Add( 0, UnityEngine.Random.Range(0.5f, 3f), 0);
+				//.Add( 0, UnityEngine.Random.Range(0.5f, 3f), 0);
+				.Add(0, 2.5f, 0);
 	}
 	
 	GameObject FindHighest(){
